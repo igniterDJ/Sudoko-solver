4b7f7c5e3e2b0a2dd874fc89dcaa4dc9f5ddf0bd
igniterDJ <72029274+igniterDJ@users.noreply.github.com>
2020-11-04 23:14:08 +0530

    Update main.c

--- main.c
+++ main.c
@@ -18,34 +18,36 @@ int sudoko[9][9]={
 */
 /*
 int sudoko[9][9]={
-                 {7,0,5,    6,1,4,    8,9,2},
-                 {8,4,2,    9,7,3,    5,6,1},
-                 {9,6,1,    2,8,5,    3,7,4},
+                 {0,3,5,    6,1,4,    8,9,2},
+                 {8,0,2,    9,7,3,    5,6,1},
+                 {0,6,1,    2,8,5,    3,7,4},
 
-                 {2,0,6,    3,4,9,    1,5,7},
+                 {2,8,6,    3,4,9,    1,5,7},
                  {4,1,3,    8,5,7,    9,2,6},
                  {5,7,9,    1,2,6,    4,3,8},
 
-                 {1,5,7,    4,9,2,    6,8,3},
-                 {6,0,4,    7,3,8,    2,1,5},
-                 {3,2,8,    5,6,1,    7,4,9}
+                 {1,5,7,    0,9,2,    6,8,3},
+                 {6,9,4,    7,0,8,    2,1,5},
+                 {3,2,8,    5,0,1,    7,4,9}
                  };
+
 */
-
-
+/*
 int sudoko[9][9]={///////////51 - 42////////
                  {5,3,0,   0,7,0,   0,0,0},
                  {6,0,0,   1,9,5,   0,0,0},
                  {0,9,8,   0,0,0,   0,6,0},
+
                  {8,0,0,   0,6,0,   0,0,3},
                  {4,0,0,   8,0,3,   0,0,1},
                  {7,0,0,   0,2,0,   0,0,6},
+
                  {0,6,0,   0,0,0,   2,8,0},
                  {0,0,0,   4,1,9,   0,0,5},
                  {0,0,0,   0,8,0,   0,7,9}
 };
 
-
+*/
 /*
 int sudoko[9][9]={   /////////////////////level 2//////////////////////////completely solved//
                  {0,1,3,   0,4,0,   0,7,0},
@@ -73,13 +75,58 @@ int sudoko[9][9]={///////////40 - 35///////////
                  {9,0,0,   1,0,0,   5,6,0}
 };
 */
+/*
+int sudoko[9][9]={
+                 {0,2,0,   0,0,0,   0,0,0},
+                 {0,0,0,   6,0,0,   0,0,3},
+                 {0,7,4,   0,8,0,   0,0,0},
+
+                 {0,0,0,   0,0,3,   0,0,2},
+                 {0,8,0,   0,4,0,   0,1,0},
+                 {6,0,0,   5,0,0,   0,0,0},
+
+                 {0,0,0,   0,1,0,   7,8,0},
+                 {5,0,0,   0,0,9,   0,0,0},
+                 {0,0,0,   0,0,0,   0,4,0}
+};
+*/
+/*
+int sudoko[9][9]={
+                 {0,0,0,   6,0,0,   4,0,0},
+                 {7,0,0,   0,0,3,   6,0,0},
+                 {0,0,0,   0,9,1,   0,8,0},
+
+                 {0,0,0,   0,0,0,   0,0,0},
+                 {0,5,0,   1,8,0,   0,0,3},
+                 {0,0,0,   3,0,6,   0,4,5},
+
+                 {0,4,0,   2,0,0,   0,6,0},
+                 {9,0,3,   0,0,0,   0,0,0},
+                 {0,2,0,   0,0,0,   1,0,0}
+};
+*/
+
+/*
+int sudoko[9][9]={
+                 {2,0,0,   3,0,0,   4,0,0},
+                 {8,0,4,   0,6,2,   6,0,0},
+                 {0,1,3,   0,9,1,   0,8,0},
+                 {,,,   ,,,   ,,},
+                 {,,,   ,,,   ,,},
+                 {,,,   ,,,   ,,},
+                 {,,,   ,,,   ,,},
+                 {,,,   ,,,   ,,},
+                 {,,,   ,,,   ,,}
+};
+*/
 int sudoko1[9][9];
 int num1[2][9];
-int sudochange1=0,sudochange2=0,sudochange3=0,sudochange4=0;
+int sudochange=0;
 int arr[9][9];
 int sudo[7][5];
 int suk[5][9];
 int ind[9][5];
+int rcb[9][9][9];
 sudokoprint()
 {
     int i,j;
@@ -115,8 +162,14 @@ sudokocheck1()
 sudokocheck2()
 {
     int i,j,k;
+
     for(j=0;j<9;j++)
     {
+        for(i=0;i<9;i++)
+        {
+            num1[1][i]=0;
+        }
+
         for(i=0;i<9;i++)
         {
             for(k=0;k<9;k++)
@@ -130,18 +183,21 @@ sudokocheck2()
 
         for(i=0;i<9;i++)
         {
+            //printf("%d\t",num1[1][i]);
             if(num1[1][i]==2)
             {
                 printf("repeated number in row\n");
                 return 1;
             }
         }
-
-        for(i=0;i<9;i++)
-        {
-            num1[1][i]=0;
-        }
+        //printf("\n");
     }
+
+    for(i=0;i<9;i++)
+    {
+        num1[1][i]=0;
+    }
+
 
     for(i=0;i<9;i++)
     {
@@ -160,10 +216,11 @@ sudokocheck2()
         {
             if(num1[1][j]==2)
             {
-                printf("repeated number in column\n");
+                //printf("repeated number in column\n");
                 return 1;
             }
         }
+        //printf("\n");
 
         for(j=0;j<9;j++)
         {
@@ -203,6 +260,7 @@ sudokocheck3()
             }
             for(k=0;k<9;k++)
             {
+                //printf("%d\t",num1[1][k]);
                 if(num1[1][k]==2)
                 {
                     printf("repeated number in box\n");
@@ -210,7 +268,7 @@ sudokocheck3()
                 }
                 num1[1][k]=0;
             }
-
+            //printf("\n");
         }
     }
 }
@@ -258,10 +316,8 @@ sudokosolve1()
                         if(num1[1][k]==0)
                         {
                             sudoko[i][j]=num1[0][k];
-                            sudochange1=1;
+                            sudochange=1;
                         }
-                        else
-                            sudochange1=2;
                     }
                 }
                 count =0;
@@ -283,7 +339,6 @@ sudokosolve2()
     for(i=0;i<9;i++)
     {
         num1[1][i]=0;
-        //printf("%d\n",num1[0][i]);
     }
 
     for(i=0;i<9;i++)
@@ -320,10 +375,8 @@ sudokosolve2()
                         if(num1[1][k]==0)
                         {
                             sudoko[i][j]=num1[0][k];
-                            sudochange2=1;
+                            sudochange=1;
                         }
-                        else
-                            sudochange2=2;
                     }
                 }
                 count =0;
@@ -389,10 +442,8 @@ sudokosolve3()
                     if(num1[1][k]==0)
                     {
                         sudoko[c][d]=num1[0][k];
-                        sudochange3=1;
+                        sudochange=1;
                     }
-                    else
-                        sudochange3=2;
                 }
             }
 
@@ -512,12 +563,10 @@ sudokosolve4()
                         if(num1[1][k]==0)
                         {
                             sudoko[i][j]=num1[0][k];
-                            sudochange4=1;
+                            sudochange=1;
                         }
                     }
                 }
-                else
-                    sudochange4=2;
                 count=0;
 
                 for(k=0;k<9;k++)
@@ -597,7 +646,7 @@ sudokosolve5()
             }
         }
 
-        if(count<=5)
+        if(count<=5 && count>1)
         {
             for(k=0;k<count;k++)
             {
@@ -617,7 +666,7 @@ sudokosolve5()
             }
         }
 
-        if(count<=5)
+        if(count<=5 && count>1)
         {
             for(j=0;j<count;j++)
             {
@@ -636,7 +685,7 @@ sudokosolve5()
                 }
                 mm++;
             }
-        }
+
 
         for(j=0;j<9;j++)
         {
@@ -654,6 +703,7 @@ sudokosolve5()
                     if(ind[j][k]==-1)
                     {
                         sudoko[sudo[0][k]][sudo[1][k]]=j+1;
+                        sudochange=1;
                     }
                 }
             }
@@ -664,7 +714,235 @@ sudokosolve5()
         index1=2;
         index=0;
         count=0;
+        }
     }
+
+}
+
+sudokosolve6()
+{
+    int i,j,k,l,index1,index2,index3,index4,a,b,count=0;
+
+    for(i=0;i<9;i++)
+    {
+        for(j=0;j<9;j++)
+        {
+            for(k=0;k<9;k++)
+            {
+                rcb[i][j][k]=0;
+            }
+        }
+    }
+
+    for(i=0;i<9;i++)
+    {
+        for(j=0;j<9;j++)
+        {
+            if(sudoko[i][j]==0)
+            {
+                for(k=0;k<9;k++)
+                {
+                    for(l=0;l<9;l++)
+                    {
+                        if(sudoko[i][k]==l+1)
+                        {
+                            rcb[i][j][l]++;
+                            //printf("%d %d %d\n",i,j,)
+                        }
+                    }
+                }
+
+                for(k=0;k<9;k++)
+                {
+                    for(l=0;l<9;l++)
+                    {
+                        if(sudoko[k][j]==l+1)
+                        {
+                            rcb[i][j][l]++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    for(a=0;a<3;a++)
+    {
+        for(b=0;b<3;b++)
+        {
+            //////////////////////
+            for(i=0;i<9;i++)
+            {
+                num1[0][i]=i+1;
+                num1[1][i]=0;
+            }
+
+//            for(i=0;i<9;i++)
+//            {
+//                printf("%d\t",num1[0][i]);
+//            }
+//            printf("\n");
+
+
+            index1=0;index2=3;index3=0;index4=3;
+            index1+=a*3;
+            index2+=a*3;
+            index3+=b*3;
+            index4+=b*3;
+            for(i=index1;i<index2;i++)
+            {
+                for(j=index3;j<index4;j++)
+                {
+                    for(k=0;k<9;k++)
+                    {
+                        if(sudoko[i][j]==num1[0][k])
+                        {
+                            num1[1][k]=1;
+                        }
+                    }
+                }
+            }
+
+            for(i=index1;i<index2;i++)
+            {
+                for(j=index3;j<index4;j++)
+                {
+                    if(sudoko[i][j]==0)
+                    {
+                        for(k=0;k<9;k++)
+                        {
+                            rcb[i][j][k]+=num1[1][k];
+                        }
+                    }
+                }
+            }
+
+            for(i=0;i<9;i++)
+            {
+                num1[0][i]=i+1;
+                num1[1][i]=0;
+            }
+
+            //////////////////////////////
+        }
+    }
+
+
+//    for(a=0;a<3;a++)
+//    {
+//        for(b=0;b<3;b++)
+//        {
+//            index1=0;index2=3;index3=0;index4=3;
+//            index1+=a*3;
+//            index2+=a*3;
+//            index3+=b*3;
+//            index4+=b*3;
+//            for(i=index1;i<index2;i++)
+//            {
+//                for(j=index3;j<index4;j++)
+//                {
+//                    for(k=0;k<9;k++)
+//                    {
+//                        printf("%d\t",rcb[i][j][k]);
+//                    }printf("\n");
+//                }
+//            }printf("\n");
+//        }
+//    }
+
+    for(a=0;a<3;a++)
+    {
+        for(b=0;b<3;b++)
+        {
+            index1=0;index2=3;index3=0;index4=3;
+            index1+=a*3;
+            index2+=a*3;
+            index3+=b*3;
+            index4+=b*3;
+            for(i=index1;i<index2;i++)
+            {
+                for(j=index3;j<index4;j++)
+                {
+                    for(k=0;k<9;k++)
+                    {
+                        if(sudoko[i][j]==num1[0][k])
+                        {
+                            num1[1][k]=1;
+                        }
+                    }
+                }
+            }
+
+            for(k=0;k<9;k++)
+                    {
+
+                            //printf("%d(%d)\t",num1[1][k],num1[0][k]);
+
+                    }
+                    //printf("\n");
+
+
+            for(k=0;k<9;k++)
+            {
+                if(num1[1][k]==0)
+                {
+                    for(i=index1;i<index2;i++)
+                    {
+                        for(j=index3;j<index4;j++)
+                        {
+                            //printf("%d\n",num1[0][k]);
+                            if(sudoko[i][j]==0)
+                            {
+                                //printf("%d %d %d %d\n",rcb[i][j][num1[0][k]-1],i,j,num1[0][k]);
+                                if(rcb[i][j][num1[0][k]-1]==0)
+                                {
+                                    //printf("%d %d %d %d\n",rcb[i][j][num1[0][k]],i,j,k);
+                                    count++;
+                                }
+                            }
+
+                        }
+
+                    }
+                    if(count==1)
+                    {
+                        for(i=index1;i<index2;i++)
+                        {
+                            for(j=index3;j<index4;j++)
+                            {
+                                //printf("%d\n",num1[0][k]);
+                                if(sudoko[i][j]==0)
+                                {
+                                    //printf("%d %d %d %d\n",rcb[i][j][num1[0][k]-1],i,j,num1[0][k]);
+                                    if(rcb[i][j][num1[0][k]-1]==0)
+                                    {
+                                        sudoko[i][j]=num1[0][k];
+                                        sudochange=1;
+                                    }
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+
+                count=0;
+            }
+
+            for(i=0;i<9;i++)
+            {
+                num1[1][i]=0;
+            }
+
+
+
+        }
+    }
+
+
+
 
 }
 
@@ -689,7 +967,6 @@ sudokorev()
 }
 int main()
 {
-
     int i,j,k,zeroct=0;
 
     for(i=0;i<9;i++)
@@ -713,28 +990,27 @@ int main()
     sudokocheck1();
     sudokocheck2();
     sudokocheck3();
-    /*do
+
+
+
+    do
     {
-        sudokosolve1();
-        sudokosolve2();
-        sudokosolve3();
-    }while(sudochange1==1 || sudochange2==1 || sudochange3==1 );*/
-
-
-    //for(i=0;i<zeroct;i++)
-    //{
+        sudochange=0;
         sudokosolve4();
-        sudokosolve4();
-        sudokosolve4();
-    //}
+        //printf("%d\n",sudochange);
+        sudokosolve5();
+        sudokorev();
+        //printf("%d\n",sudochange);
+        sudokosolve5();
+        sudokorev();
+        //printf("%d\n",sudochange);
+        sudokosolve6();
+        //printf("%d\n",sudochange);
 
-    sudokosolve5();
-    sudokorev();
-    sudokosolve5();
-    sudokorev();
+    }while(sudochange>=1);
 
-    sudokosolve4();
-    sudokosolve4();
+
+
     zeroct=0;
     for(i=0;i<9;i++)
     {
@@ -748,6 +1024,11 @@ int main()
     }
     printf("final no of blanks are %d\n",zeroct);
     sudokoprint();
+
+    sudokocheck1();
+    sudokocheck2();
+    sudokocheck3();
+
 
 
 }
